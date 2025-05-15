6c640e908e5398a8bb7ff5833687fda2e7b98004
Xepobopa <ismartsdn4477@gmail.com>
2025-05-15 16:13:17 +0200

    Update

--- src/common/service/product/product.ts
+++ src/common/service/product/product.ts
@@ -5,56 +5,28 @@ const HOST = process.env.REACT_APP_HOST;
 
 export class ProductService {
     static async create(formData: FormData) {
-        return await axios.post(`${HOST}/api/product/create`, formData, {
-            // headers: {
-            //     'Content-Type': 'multipart/form-data',
-            //     'Access-Control-Allow-Origin': '*',
-            // }
-        })
+        return await axios.post(`${HOST}/api/product/create`, formData)
     }
 
     static async getAllWithParents() {
-        return await axios.get(`${HOST}/api/product/getAll`, {  
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-            }
-        });
+        return await axios.get(`${HOST}/api/product/getAll`);
     }
 
     static async getAll(isCatalog?: boolean) {
         return await axios.get<TProduct[]>(`${HOST}/api/product/getAll`, {  
             params: { isCatalog },
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-            }
         });
     }
 
     static async getProduct(id: number) {
-        return await axios.get(`${HOST}/api/product/getOne/${id}`, {  
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-            }
-        });
+        return await axios.get(`${HOST}/api/product/getOne/${id}`);
     }
 
     static async update(id: number, formData: FormData) {
-        return await axios.patch(`${HOST}/api/product/update/${id}`, formData, {  
-            // headers: {
-            //     'Content-Type': 'multipart/form-data',
-            //     'Access-Control-Allow-Origin': '*',
-            // }
-        });
+        return await axios.patch(`${HOST}/api/product/update/${id}`, formData);
     }
 
     static async delete(id: number) {
-        return await axios.delete(`${HOST}/api/product/${id}`, {  
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-            }
-        });
+        return await axios.delete(`${HOST}/api/product/${id}`);
     }
 }
